67d4658c4fd174771a0ceae7b64281b89ac9d893
rajamanohar <rajamanohar.mummidi@gmail.com>
2015-10-19 16:00:13 -0400

    ListE implemention pushing to origin

--- Program1.m
+++ Program1.m
@@ -21,16 +21,11 @@ fprintf('Output hypothesis from the Find-S Alogirthm\n')
 disp(h);
 
 % % % % % % % % % % % ListE alogirthm  % % % % % % % % % % % % % % % %  % % % % % %
-
-
-
-
-
-% % % % % % % % % % % Candidate Alogrithm  % %  % % %  % % % %
-S=[0,0,0,0];
+S=[nan,nan,nan,nan];
 G=[-1,-1,-1,-1];
 
 AttrMat=Desc_Mat(:,1:4);
+% Assigining Most General value to Attributes 
 AttrMat(row+1,1:4)=G;
 
 matAttr1=unique(AttrMat(:,1));
@@ -38,6 +33,8 @@ matAttr2=unique(AttrMat(:,2));
 matAttr3=unique(AttrMat(:,3));
 matAttr4=unique(AttrMat(:,4));
 r=1;
+
+% Finding all possible hypothises from the given data set.
 for idx1=1:length(matAttr1)
     for idx2=1:length(matAttr2)
         for idx3=1:length(matAttr3)
@@ -50,8 +47,40 @@ for idx1=1:length(matAttr1)
     end
 end
 [r c]=size(H);
+% Assigining Most Specific value to Attributes 
 H(r+1,1:4)=S;
 
+% initializing the version space with set of all possible hypothises 
+VS=H;
+
+for idxTD=1:length(TrainData)
+    
+    for idxVS=1:length(VS)
+        temp=0;
+        if(VS(idxVS,1)~=TrainData(idxTD,1) && VS(idxVS,1)~=-1)
+          temp=1;  
+        end
+        if (VS(idxVS,2)~=TrainData(idxTD,2) && VS(idxVS,2)~=-1)
+            temp=1;
+        end
+        if (VS(idxVS,3)~=TrainData(idxTD,3) && VS(idxVS,3)~=-1)
+            temp=1;
+        end
+        
+        if (VS(idxVS,4)~=TrainData(idxTD,4) && VS(idxVS,4)~=-1)
+            temp=1;
+        end
+        
+        if(temp==1)
+           VS(idxVS,:)=nan;
+        end
+    end
+end
+
+% Removing the hypotheses  which are inconsistent with training data 
+VS(~any(isnan(VS),2),:);
+fprintf('\n VersionSpace that is consistent with the training data is \n');
+disp(VS);
 
 
 
--- findsAlgorithm.m
+++ findsAlgorithm.m
@@ -13,15 +13,15 @@ for idx=1:length(TrainData)
         if(h(1,1)~=TrainData(idx,1) && h(1,1)~=-1)
             h(1,1)=-1;
         end
-        if (h(1,2)~=TrainData(idx,2) && h(1,1)~=-1)
-            h(1,1)=-1;
+        if (h(1,2)~=TrainData(idx,2) && h(1,2)~=-1)
+            h(1,2)=-1;
         end
-        if (h(1,3)~=TrainData(idx,3) && h(1,1)~=-1)
-            h(1,1)=-1;
+        if (h(1,3)~=TrainData(idx,3) && h(1,3)~=-1)
+            h(1,3)=-1;
         end
         
-        if (h(1,4)~= TrainData(idx,4) && h(1,1)~=-1)
-            h(1,1)=-1;
+        if (h(1,4)~= TrainData(idx,4) && h(1,4)~=-1)
+            h(1,4)=-1;
         end
     end
     
