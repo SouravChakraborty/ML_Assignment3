14278f50733295e5bce006f3da046cec17854e85
rajamanohar <rajamanohar.mummidi@gmail.com>
2015-10-20 12:06:15 -0400

    completed Candidate algorithm

--- Program1.m
+++ Program1.m
@@ -23,7 +23,7 @@ disp(h);
 % % % % % % % % % % % List then Eliminate alogirthm  % % % % % % % % % % % % % % % %  % % % % % %
 
 
-[VS]=ListE(Desc_Mat,TrainData)
+[VS]=ListE(Desc_Mat,TrainData);
 fprintf('\n VersionSpace that is consistent with the training data is \n');
 disp(VS);
 
@@ -31,17 +31,18 @@ disp(VS);
 % % % % % % % % % % % Candidate alogirthm  % % % % % % % % % % % % % % % %  % % % % % %
 S=[nan,nan,nan,nan];
 G=[-1,-1,-1,-1];
+[tr tc]=size(TrainData);
 
-for index=1:length(TrainData)
+for index=1:tr
     
     if(TrainData(index,5)==1)
         if(sum(isnan(S))==4)
             S(1,:)=TrainData(index,1:4);
         end
-%        Removing inconsistencies from G
-            [G]=removeInconsistentRows(G,TrainData(index,:))
+        %        Removing inconsistencies from G
+        [G]=removeInconsistentRows(G,TrainData(index,:));
         
-%         Values 
+        %         Values
         if(S(1,1)~=TrainData(index,1) && S(1,1)~=-1)
             S(1,1)=-1;
         end
@@ -51,7 +52,6 @@ for index=1:length(TrainData)
         if (S(1,3)~=TrainData(index,3) && S(1,3)~=-1)
             S(1,3)=-1;
         end
-        
         if (S(1,4)~= TrainData(index,4) && S(1,4)~=-1)
             S(1,4)=-1;
         end
@@ -59,35 +59,113 @@ for index=1:length(TrainData)
         
         % Eliminating all General hypotheses which are inconsitent
         
-      
-        
-        
     else
-%      Remove from S any hypothesis inconsistent with d
-          [S]=removeInconsistentRows(S,TrainData(index,:))
-
-        [r c]=size(G);
+        % Remove from S any hypothesis inconsistent with d
         
-        for idxG=1:length(G)
-     
-            if(G(idxG,1)==-1)
-                r=r+1;
-                G(r,:)=[S(1,1),G(1,2),G(1,3),G(1,4)];
-                
+        [S]=removeInconsistentRows(S,TrainData(index,:));
+        %  Replacing any hypothesis in G with minimal specializations
+        [Grws c]=size(G);
+        
+        for idxG=1:Grws
+            [grc ,rc]=size(G);
+            if(idxG>grc)
+                break;
             end
-            if( G(idxG,2)~=-1)
-                r=r+1;
-                G(r,:)=[G(1,1),S(1,2),G(1,3),G(1,4)];
+            temp=0;
+            if((G(idxG,1)==TrainData(index,1)) || (G(idxG,1)==-1))
+                temp=temp+1;
             end
-            if(G(idxG,3)~=-1)
-                r=r+1;
-                G(r,:)=[G(1,1),G(1,2),S(1,3),G(1,4)];
+            if(G(idxG,2)==TrainData(index,2) || G(idxG,2)==-1)
+                temp=temp+1;
             end
-            if(G(idxG,4)~=-1)
-                r=r+1;
-                G(r,:)=[G(1,1),G(1,2),S(1,3),G(1,4)];
+            if(G(idxG,3)==TrainData(index,3) || G(idxG,3)==-1)
+                temp=temp+1;
+            end
+            if(G(idxG,4)==TrainData(index,4) || G(idxG,4)==-1)
+                temp=temp+1;
             end
             
+            
+            if(temp==4)
+                GgenIndex=find(G(idxG,:)==-1);
+                if(~isempty(S))
+                SgenIndex=find(S(1,:)==-1);
+                end
+                
+                R=TrainData(index,GgenIndex(1,:));
+                
+                
+                AttrMat=Desc_Mat(:,1:4);
+                % Assigining Most General value to Attributes
+                AttrMat(row+1,1:4)=[-1,-1,-1,-1];
+                
+                matAttr1=unique(AttrMat(:,1));
+                matAttr2=unique(AttrMat(:,2));
+                matAttr3=unique(AttrMat(:,3));
+                matAttr4=unique(AttrMat(:,4));
+                r=1;
+                
+                % Finding all possible hypothises from the given data set.
+                for idx1=1:length(matAttr1)
+                    for idx2=1:length(matAttr2)
+                        for idx3=1:length(matAttr3)
+                            for idx4=1:length(matAttr4)
+                                H(r,:)=[matAttr1(idx1),matAttr2(idx2),matAttr3(idx3),matAttr4(idx4)];
+                                
+                                r=r+1;
+                            end
+                        end
+                    end
+                end
+                
+                [rh ch]=size(H);
+                for inx=1:rh
+                    if(nnz(H(inx,:)==-1)~=length(GgenIndex)-1)
+                        H(inx,:)=nan;
+                    end
+                    
+                    for len=1:length(R)
+                        if(H(inx,len)==R(1,len))
+                            H(inx,:)=nan;
+                            break;
+                        end
+                    end
+                    for len=1:length(S)
+                        if(S(1,len)==-1 && H(inx,len)~=S(1,len))
+                            H(inx,:)=nan;
+                            break;
+                        end
+                    end
+                    
+                end
+                
+                  H=H(~any(isnan(H),2),:);
+              
+                  G(idxG,:)=nan;
+                  G=G(~any(isnan(G),2),:);
+                  G=vertcat(G,H);
+                  G=unique(G,'rows');
+                  
+                 [ wr wc]=size(G);
+                 rlength=0;
+                 
+                  for irdx=1:wr
+                      if(rlength==0)
+                      rlength=nnz(G(irdx,:)==-1);
+                      end
+                      if(rlength<=nnz(G(irdx,:)==-1))
+                           rlength=nnz(G(irdx,:)==-1);
+                      else 
+                        G(irdx,:)=nan;  
+                      end
+                  end
+                  
+                    G=G(~any(isnan(G),2),:);
+                   
+                    
+            end
+            
+          
             
         end
         
@@ -95,11 +173,9 @@ for index=1:length(TrainData)
     end
 end
 
-% Selection of training data from the iris dataset
+fprintf('\n Specification hypotheses set for given dataset in Candidate Algorithm \n');
+disp(S);
+fprintf('\n Generalization hypotheses set for given dataset in Candidate Algorithm \n');
+disp(G);
 
-% List then Elimination example
 
-
-
-% d is positive example
-% d is negative example
--- removeInconsistentRows.m
+++ removeInconsistentRows.m
@@ -1,28 +1,35 @@
-function[h]=removeInconsistentRows(h,TrainData)
-for idxh=1:length(h)
+function[h]=removeInconsistentRows(h,TData)
+[rwh,c]=size(h);
+
+for idxh=1:rwh
     temp=0;
+     [rwhc,c]=size(h);
+            if(idxh>rwhc)
+                break;
+            end
+    if(h(idxh,1)~=TData(1,1)  && h(idxh,1)~=-1)
+        temp=1;
+    end
+    if (h(idxh,2)~=TData(1,2) && h(idxh,2)~=-1)
+        temp=1;
+    end
+    if (h(idxh,3)~=TData(1,3) && h(idxh,3)~=-1)
+        temp=1;
+    end
     
-    if(h(idxh,1)~=TrainData(1,1)  && h(idxh,1)~=-1)
-        temp=1;
-    end
-    if (h(idxh,2)~=TrainData(1,2) && h(idxh,2)~=-1)
-        temp=1;
-    end
-    if (h(idxh,3)~=TrainData(1,3) && h(idxh,3)~=-1)
+    if (h(idxh,4)~=TData(1,4) && h(idxh,4)~=-1)
         temp=1;
     end
     
-    if (h(idxh,4)~=TrainData(1,4) && h(idxh,4)~=-1)
-        temp=1;
-    end
-    
-    if(temp==1 && TrainData(1,5)==1)
-        h(idxVS,:)=nan;
-    elseif(temp~=1 && TrainData(1,5)~=1)
-        h(idxVS,:)=nan;
+    if(temp==1 && TData(1,5)==1)
+        h(idxh,:)=nan;
+        h=h(~any(isnan(h),2),:);
+    elseif(temp~=1 && TData(1,5)~=1)
+        h(idxh,:)=nan;
+        h=h(~any(isnan(h),2),:);
     end
 end
 
-h=h(~any(isnan(h),2),:);
+
 end
 
